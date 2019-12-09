c2174934080fe1e7923217f34be6d90a532d4edb
Matthew C <m23.cullen@gmail.com>
2019-12-09 09:47:02 +0000

    Automatically backed up by Learn

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
+<<<<<<< HEAD
   self.abstract_class = true
 end
+=======
+  #self.abstract_class = true
+end
+>>>>>>> 91ef2877a0ede11ff9ff10425575268658ea233c
